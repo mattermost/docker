7d565855e5578b76df21835624cd2859e87348ff
ngocdoan <vuongngocdoan@gmail.com>
2023-03-03 11:31:56 +0700

    Update restart-mm-docker.sh

--- restart-mm-docker.sh
+++ restart-mm-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt update && sudo apt upgrade -y
 sudo docker-compose -f docker-compose.yml -f docker-compose.nginx.yml down
 sleep 5
 sudo docker-compose -f docker-compose.yml -f docker-compose.nginx.yml up -d
