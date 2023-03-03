eed6830c794f5dabced548795adab522f62ee057
ngocdoan <vuongngocdoan@gmail.com>
2023-03-03 10:20:13 +0700

    Create 1-click-install-without-nginx.sh

---
+++ 1-click-install-without-nginx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update system
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl build-essential git wget jq make gcc ack tmux ncdu -y
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+
+# install docker
+apt update && apt install git sudo unzip wget -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+#
+cp env.example .env
+mkdir -p ./volumes/app/mattermost/{config,data,logs,plugins,client/plugins,bleve-indexes}
+sudo chown -R 2000:2000 ./volumes/app/mattermost
+
+sudo docker-compose -f docker-compose.yml -f docker-compose.without-nginx.yml up -d
+
+
